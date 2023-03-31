756445635beea4005a2d81324eeddcd30775188b
rahul-vishwakarma-05 <116077468+rahul-vishwakarma-05@users.noreply.github.com>
2023-03-31 20:55:51 +0530

    Update love.js

--- love.js
+++ love.js
@@ -1,12 +1,19 @@
-const btn = document.querySelector('button');
+const btn        = document.querySelector('button');
+
 
 btn.addEventListener('click' , () =>{
   const random_num = Math.random() * 100;
   const random = Math.floor(random_num);
 
   const show_result = document.querySelector('.percentage > input');
-   
+  
+  const male_input = document.querySelector('.male_name > input');
+  const female_input = document.querySelector('.female_name > input');
 
-  show_result.value = random + " % ";
-
+  if (male_input.value == '' || female_input.value == '') {
+   alert('plz enter the names');
+  }else{
+    show_result.value = random + " % "; 
+  }
+  
 });
